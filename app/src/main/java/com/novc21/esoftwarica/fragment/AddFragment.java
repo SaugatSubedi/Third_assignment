414d7f9fd7834aaf05d1797f9a02553755b76af0
Saugat Subedi <subedisaugat176@gmail.com>
2019-12-26 21:21:50 +0545

    added home, add student, about fragments added

--- app/src/main/java/com/novc21/esoftwarica/fragment/AddFragment.java
+++ app/src/main/java/com/novc21/esoftwarica/fragment/AddFragment.java
@@ -31,3 +31,63 @@ public class AddFragment extends Fragment implements RadioGroup.OnCheckedChangeL
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+// Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        etName = view.findViewById(R.id.etName);
+        etAge = view.findViewById(R.id.etAge);
+        etAddress = view.findViewById(R.id.etAddress);
+        rgGender = view.findViewById(R.id.rgGender);
+        btnSave = view.findViewById(R.id.btnSave);
+        rgGender.setOnCheckedChangeListener(this);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate()){
+                    name = etName.getText().toString();
+                    address = etAddress.getText().toString();
+                    age = Integer.parseInt(etAge.getText().toString());
+                    MainActivity.studentsList.add(new Students(name,gender,address,age));
+                    Toast.makeText(getContext(), "Added successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return view;
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == R.id.rbMale) {
+            gender = "male";
+        }
+        if (checkedId == R.id.rbFemale) {
+            gender = "female";
+        }
+        if (checkedId == R.id.rbOther) {
+            gender = "other";
+        }
+    }
+
+    private boolean validate() {
+        if (TextUtils.isEmpty(etName.getText())) {
+            etName.setError("Enter full name");
+            etName.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(etAge.getText())) {
+            etAge.setError("Enter the age");
+            etAge.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(etAddress.getText())) {
+            etAddress.setError("Enter the address");
+            etAddress.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(gender)) {
+            Toast.makeText(getContext(), "Select gender", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+}
+
